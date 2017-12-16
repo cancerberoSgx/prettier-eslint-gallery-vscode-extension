c40498c534184afbf89266226d47bcbff4bb85ba
cancerberosgx <sebastigurin@gmail.com>
2017-12-15 23:05:43 -0300

    fixed prettier-eslint-gallery as dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 ## 0.0.1
 - Initial contribution - working but not settings
 - Settings
+- fixed prettier-eslint-gallery as dependency
--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,46 @@
 import * as vscode from 'vscode';
 import { Range, Position } from 'vscode';
-const formatter = require('prettier-eslint-gallery');
+
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('extension.prettier-eslint-gallery', () => {
-    let editor = vscode.window.activeTextEditor;
-    let extensionConfig = vscode.workspace.getConfiguration('prettier-eslint-gallery');
-    let config = Object.assign({ source: editor.document.getText() }, extensionConfig);
-    let result;
-    try {
-      result = formatter(config);
-    } catch (ex) {
-      vscode.window.showErrorMessage('Cannot format code with syntax error' + ex);
-      return;
-    }
-    editor.edit(editorEdit => {
-      editorEdit.replace(
-        new Range(
-          editor.document.lineAt(0).range.start,
-          editor.document.lineAt(editor.document.lineCount - 1).range.end,
-        ),
-        result,
-      );
-    });
+    formatCurrentEditor();
   });
-
   context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
+
+
+const formatter = require('prettier-eslint-gallery');
+export function formatCurrentEditor(){
+  let editor = vscode.window.activeTextEditor;
+  let extensionConfig = vscode.workspace.getConfiguration('prettier-eslint-gallery');
+  let config = Object.assign({ source: editor.document.getText() }, extensionConfig);
+  let result;
+  try {
+    result = formatter(config);
+  } catch (ex) {
+    let errMessage = 'Cannot format code with syntax error' + ex
+    vscode.window.showErrorMessage(errMessage);
+    console.log(errMessage)
+    console.log(ex.stack)
+    debugger
+    return;
+  }
+  editorReplaceText(editor, result)
+}
+
+
+export function editorReplaceText(editor, result, done=undefined){
+  editor.edit(editorEdit => {
+    editorEdit.replace(
+      new Range(
+        editor.document.lineAt(0).range.start,
+        editor.document.lineAt(editor.document.lineCount - 1).range.end,
+      ),
+      result,
+    );
+    done && done()
+  });
+}
